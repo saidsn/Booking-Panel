e3397a40f777e1a63f48fca5461df613260563c9
saidsn <saidsn@code.edu.az>
2023-08-12 15:00:10 +0400

    Create SideBar and Main section

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const staffList = document.querySelector('.staff-list');
+
+staff.forEach(staffMember => {
+    const staffCard = document.createElement('div');
+    staffCard.classList.add('staff-card');
+    staffCard.innerHTML = `
+        <img src="${staffMember.image}" alt="${staffMember.name}">
+        <h2>${staffMember.name}</h2>
+        <p>${staffMember.email}</p>
+        <button class="select-button" data-id="${staffMember.id}">Select</button>
+    `;
+    staffList.appendChild(staffCard);
+});
+
+staffList.addEventListener('click', event => {
+    if (event.target.classList.contains('select-button')) {
+        const selectedStaffId = parseInt(event.target.getAttribute('data-id'));
+        // Handle the selected staff member and proceed to the next step
+    }
+});
